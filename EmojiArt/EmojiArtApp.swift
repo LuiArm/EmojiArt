1a64a5b359e35f697f1a96345b6ddc1225acd7a0
LuiArm <Luisarmendariz24@icloud.com>
2024-02-29 12:01:42 -0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by luis armendariz on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
