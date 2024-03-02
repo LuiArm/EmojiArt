c19fdd49683a407c68726de038cf60b6a536288b
LuiArm <Luisarmendariz24@icloud.com>
2024-03-01 22:19:07 -0700

    model and vm

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -1,8 +1,29 @@
 //
 //  EmojiArtDocument.swift
-//  EmojiArt
+//  EmojiArtDocument view model
 //
 //  Created by luis armendariz on 3/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    typealias Emoji = EmojiArt.Emoji
+    
+    private var emojiArt = EmojiArt()
+    
+    var emojis: [Emoji] {
+        emojiArt.emojis
+    }
+    
+    var background: URL? {
+        emojiArt.background
+    }
+    
+    // MARK: - Intent(S)
+    
+    func setBackground(_ url: URL?) {
+        emojiArt.background = url
+    }
+    
+}
