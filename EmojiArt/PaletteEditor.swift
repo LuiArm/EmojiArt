1b793379e41a1e5b5211c2e5da5f4af81728600a
LuiArm <luisarmendariz24@icloud.com>
2024-04-12 22:41:10 -0600

    Palette Editor View

---
+++ EmojiArt/PaletteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by luis armendariz on 4/12/24.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteEditor()
+}
